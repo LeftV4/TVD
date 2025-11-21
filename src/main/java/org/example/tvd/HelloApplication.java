28e85e451de55e6f8094827614a6d673ce0bc30a
Lefteris Vourvou <leftvour@gmail.com>
2025-11-21 23:07:07 +0200

    Initial commit

---
+++ src/main/java/org/example/tvd/HelloApplication.java
@@ -0,0 +1,21 @@
+package org.example.tvd;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/layout.fxml")));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Hotel Management System");
+        primaryStage.show();
+    }
+}
